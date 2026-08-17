8ef49c2e964decc7c47b6d85bf31754ff4a9ac75
Aemjirapat.tr <aemjirapat.tr@gmail.com>
2026-08-17 23:36:32 +0700

    refactor: remove work event use cases

--- src/core/work-events/application/use-cases/get-work-event-summary.use-case.ts
+++
@@ -1,9 +0,0 @@
-import type { WorkEventRepository } from '../../domain/work-event.repository';
-
-export class GetWorkEventSummaryUseCase {
-  constructor(private readonly workEventRepository: WorkEventRepository) {}
-
-  execute() {
-    return this.workEventRepository.getSummary();
-  }
-}
--- src/core/work-events/work-events.module.ts
+++ src/core/work-events/work-events.module.ts
@@ -1,6 +1,3 @@
-import { CreateWorkEventUseCase } from './application/use-cases/create-work-event.use-case';
-import { GetWorkEventSummaryUseCase } from './application/use-cases/get-work-event-summary.use-case';
-import { ListWorkEventsUseCase } from './application/use-cases/list-work-events.use-case';
 import { MongooseWorkEventRepository } from './infrastructure/repository/work-event.repository';
 import { WorkEventService } from './infrastructure/services/work-event.service';
 
@@ -14,11 +11,6 @@ export const createWorkEventsModule = () => {
     },
     services: {
       workEventService,
-    },
-    useCases: {
-      createWorkEvent: new CreateWorkEventUseCase(workEventRepository),
-      listWorkEvents: new ListWorkEventsUseCase(workEventRepository),
-      getWorkEventSummary: new GetWorkEventSummaryUseCase(workEventRepository),
     },
   };
 };
