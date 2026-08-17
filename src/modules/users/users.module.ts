4283dd70c168eb18a07a5b93651bc353eb1cc8e3
Aemjirapat.tr <aemjirapat.tr@gmail.com>
2026-08-18 00:16:12 +0700

    refactor: remove route post on user module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -42,36 +42,8 @@ export const createUsersRouter = (): Router => {
    *     responses:
    *       200:
    *         description: User list
-   *   post:
-   *     summary: Create user
-   *     tags:
-   *       - Users
-   *     requestBody:
-   *       required: true
-   *       content:
-   *         application/json:
-   *           schema:
-   *             type: object
-   *             required:
-   *               - first_name
-   *               - last_name
-   *               - phone_number
-   *             properties:
-   *               first_name:
-   *                 type: string
-   *               last_name:
-   *                 type: string
-   *               phone_number:
-   *                 type: string
-   *               is_active:
-   *                 type: boolean
-   *                 default: true
-   *     responses:
-   *       201:
-   *         description: User created
    */
   router.get('/', controller.findAllAndCounted);
-  router.post('/', controller.createUser);
 
   return router;
 };
